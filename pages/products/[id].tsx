2c7e96f1e6aee2ef82ee23c9303c01ac92eae08b
Tommy <tommy.alfarabi@gmail.com>
2023-02-11 02:55:29 +0700

    penambahan fallback

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -10,7 +10,7 @@ export async function getStaticPaths() {
         paths: products.map((product) => ({
             params: { id: product.id.toString() },
         })),
-        fallback: false
+        fallback: 'blocking',
     };
 }
 
